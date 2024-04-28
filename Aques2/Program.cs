fc2d0a7a23d911e2442f41b664b51408288263dc
Hector Caudel <hector.caudel@tribal.credit>
2024-04-27 22:53:14 -0600

    first commit

---
+++ Aques2/Program.cs
@@ -0,0 +1,60 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace Aques2 {
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sprint = new int[] { 3, 2 };
+            int k = 3;
+            Aquestion item = new Aquestion();
+
+            Console.WriteLine(item.GetMaximumPoints(sprint, k));
+        }
+    }
+
+
+    public class Aquestion
+    {
+
+        public int GetMaximumPoints(int[] days, int k)
+        {
+            List<int> daysExpanded = new List<int>();
+
+            foreach (int item in days)
+            {
+                for (int x = 1; x <= item; x++)
+                    daysExpanded.Add(x);
+            }
+
+            int maxPoints = 0;
+
+            for (int x = 0; x < daysExpanded.Count; x++)
+            {
+                int current = 0;
+                int valueDay = 0;
+                int edge = 0;
+
+                for (int y = 0; y < k; y++)
+                {
+                    if ((x + y) > (daysExpanded.Count - 1))
+                    {
+                        valueDay = daysExpanded[edge];
+                        edge++;
+                    }
+                    else
+                    {
+                        valueDay = daysExpanded[x + y];
+                    }
+                    current += valueDay;
+                }
+
+                if (current > maxPoints)
+                    maxPoints = current;
+            }
+
+            return maxPoints;
+        }
+
+    }
+}
---
+++ testAques2/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using Aques2;
+using Xunit;
+
+
+namespace testAques2;
+
+public class UnitTest1
+{
+    [Theory]
+    [InlineData(new int[]{3,2},3,6)]
+    [InlineData(new int[] {3,5,6,2 },5,20)]
+    public void TestGetMaximumPoints(int[] days, int k, int expectedResult)
+    {
+        //Act
+        Aquestion item = new Aquestion();
+
+        //Assert
+        int result = item.GetMaximumPoints(days, k);
+
+        Assert.Equal(expectedResult, result);
+    }
+}
